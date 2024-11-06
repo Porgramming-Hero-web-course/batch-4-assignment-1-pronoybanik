e4e9b245820c4d342375a076d1b73a034f952d56
PRONOY <pronoybanik82@gmail.com>
2024-11-06 20:54:27 +0600

    solve problem-1

--- src/problem-1.ts
+++ src/problem-1.ts
@@ -1,1 +1,11 @@
-console.log("please print");
+{
+
+    const sumArray = (array: number[]): number => {
+
+        const sumValue = array.reduce((value, currentValue) => value + currentValue, 0)
+        return sumValue;
+    }
+    const result = sumArray([1, 2, 3, 4, 5]);
+    console.log(result);
+
+}
